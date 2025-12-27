481d4d60a96c6687dd28fead1dd9efbbd2fb66c8
Kishor Neupane <neupkishor.7@gmail.com>
2025-12-27 18:49:18 +0000

    make sitemap.xml make this page now.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next';
+
+const URL = 'https://neupgroup.com';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const routes = [
+    '/',
+    '/about/teams',
+    '/about/ventures',
+    '/design',
+    '/jobs',
+    '/legal',
+    '/legis',
+    '/mail',
+    '/marketing',
+    '/neupid',
+    '/socials',
+    '/tourio',
+  ];
+
+  return routes.map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date(),
+  }));
+}
