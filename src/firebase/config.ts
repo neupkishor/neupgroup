b92d0935896e57c707706ef26be9a5d5135e9d6f
Kishor Neupane <neupkishor.7@gmail.com>
2025-12-27 19:11:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7089379111-9b59d",
+  "appId": "1:543120277370:web:e814fc5dbd6325c3a720c3",
+  "apiKey": "AIzaSyBZwIUy6DW_Ahll6XxL51lpbwRxmxim0Mg",
+  "authDomain": "studio-7089379111-9b59d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "543120277370"
+};
