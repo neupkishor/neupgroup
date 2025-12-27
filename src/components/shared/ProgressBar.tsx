a11b7cbb4093bd9458e3b0973b534c087f56f09e
Kishor Neupane <neupkishor.7@gmail.com>
2025-12-27 17:42:04 +0000

    no, immediately on the click being listened, start the nprogress and whe

--- src/components/shared/ProgressBar.tsx
+++ src/components/shared/ProgressBar.tsx
@@ -4,56 +4,77 @@ import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import NProgress from 'nprogress';
 
-export function ProgressBar() {
+// A component that subscribes to route changes and triggers NProgress.
+// This is a bit of a hack, but it's a common way to implement this
+// with the Next.js App Router, as of Next.js 13.
+//
+// See: https://github.com/vercel/next.js/discussions/41934
+function NProgressEvents() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    NProgress.configure({ showSpinner: false });
-
-    const handleStart = () => NProgress.start();
-    const handleStop = () => NProgress.done();
-
-    handleStop(); // Stop progress on initial load
-
-    return () => {
-      handleStop();
-    };
-  }, []);
-
-  useEffect(() => {
     NProgress.done();
+    return () => {
+      // NProgress.start();
+    };
   }, [pathname, searchParams]);
 
-  // This is a component that will be used to wrap around links
-  // to trigger the progress bar.
-  // We are monkey patching the Next.js Link component to trigger nprogress.
-  // This is a bit of a hack, but it's the best way to do it for now.
+  return null;
+}
+
+
+export function ProgressBar() {
   useEffect(() => {
-    const originalPushState = history.pushState;
-    history.pushState = function (...args) {
-      NProgress.start();
-      return originalPushState.apply(history, args);
+    NProgress.configure({ showSpinner: false });
+
+    const originalFetch = window.fetch;
+    window.fetch = async function (...args) {
+      // Don't show the progress bar for Genkit flow requests.
+      if (typeof args[0] === 'string' && args[0].includes('/flows/')) {
+        return originalFetch(...args);
+      }
+      if (NProgress.isStarted() === false) {
+        NProgress.start();
+      }
+      try {
+        return await originalFetch(...args);
+      } catch (error) {
+        NProgress.done();
+        throw error;
+      }
     };
 
-    const originalReplaceState = history.replaceState;
-    history.replaceState = function (...args) {
-      NProgress.start();
-      return originalReplaceState.apply(history, args);
-    };
+    // This is a workaround for a bug in Next.js where the progress bar
+    // doesn't finish when the user navigates to a page with a full
+    // page loading component.
+    const MutationObserver = window.MutationObserver;
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        for (const node of Array.from(mutation.addedNodes)) {
+          // Check if the added node is the loading-indicator
+          if (
+            node instanceof HTMLElement &&
+            node.getAttribute('data-next-router-loading') === ''
+          ) {
+            NProgress.done();
+            return;
+          }
+        }
+      }
+    });
 
-    const handlePopState = () => {
-      NProgress.start();
-    };
-
-    window.addEventListener('popstate', handlePopState);
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
 
     return () => {
-      history.pushState = originalPushState;
-      history.replaceState = originalReplaceState;
-      window.removeEventListener('popstate', handlePopState);
+      window.fetch = originalFetch;
+      observer.disconnect();
     };
   }, []);
 
-  return null;
+
+  return <NProgressEvents />;
 }
